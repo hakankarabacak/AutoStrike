7a4582b9cf3555a88f75dbfe88c6f8342836f5de
Mehmet Bilgiç <2611mehmet@gmail.com>
2024-01-07 17:31:41 +0300

    proje_son_guncelleme

---
+++ javascript/createattack.js
@@ -0,0 +1,57 @@
+function checkbox_write2() {
+    // Get the checkbox
+    var checkBox = document.getElementById("checkbox2");
+    // Get the output text
+    var text = document.getElementById("checkbox2_text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+
+function checkbox_write1() {
+    // Get the checkbox
+    var checkBox = document.getElementById("checkbox1");
+    // Get the output text
+    var text = document.getElementById("checkbox1_text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function checkbox_write3() {
+    // Get the checkbox
+    var checkBox = document.getElementById("checkbox3");
+    // Get the output text
+    var text = document.getElementById("checkbox3_text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function checkbox_write4() {
+    // Get the checkbox
+    var checkBox = document.getElementById("checkbox4");
+    // Get the output text
+    var text = document.getElementById("checkbox4_text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
